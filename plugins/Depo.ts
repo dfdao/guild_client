939084d4232e911265de7432b33bef134ef017f4
Doug Binder <douglasjbinder@gmail.com>
2022-02-22 23:39:47 -0600

    feat: basic views scaffolded out (blocked by test data)

--- plugins/Depo.ts
+++ plugins/Depo.ts
@@ -1,311 +1,113 @@
-import { Depo } from "./depo-contract";
-import { BigNumber as EthersBN } from "ethers";
-import {
-  artifactIdFromEthersBN,
-  artifactIdToDecStr,
-} from "@darkforest_eth/serde";
-import { DarkForest } from "@darkforest_eth/contracts/typechain";
-import { ArtifactId } from "@darkforest_eth/types";
+import { Artifact, ArtifactType } from "@darkforest_eth/types";
+//@ts-ignore
+import { initializeContract } from "./depo/lib";
+import { Button } from "./views/basics";
+type Pane = "withdraw" | "deposit" | "loading";
 
-const DEPO_ABI = [
-  {
-    type: "constructor",
-    inputs: [
-      {
-        internalType: "address",
-        name: "core",
-        type: "address",
-      },
-      {
-        internalType: "address[]",
-        name: "blessed",
-        type: "address[]",
-      },
-    ],
-  },
-  {
-    type: "function",
-    name: "captain",
-    inputs: [
-      {
-        internalType: "address",
-        name: "pleb",
-        type: "address",
-      },
-    ],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    type: "function",
-    name: "captains",
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-  },
-  {
-    type: "function",
-    name: "demote",
-    inputs: [
-      {
-        internalType: "address",
-        name: "pleb",
-        type: "address",
-      },
-    ],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    type: "function",
-    name: "deposits",
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-  },
-  {
-    type: "function",
-    name: "dfCore",
-    inputs: [],
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-  },
-  {
-    type: "function",
-    name: "masterAtArms",
-    inputs: [
-      {
-        internalType: "address",
-        name: "pleb",
-        type: "address",
-      },
-    ],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    type: "function",
-    name: "mastersAtArms",
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-  },
-  {
-    type: "function",
-    name: "onERC721Received",
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "from",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "tokenId",
-        type: "uint256",
-      },
-      {
-        internalType: "bytes",
-        name: "",
-        type: "bytes",
-      },
-    ],
-    outputs: [
-      {
-        internalType: "bytes4",
-        name: "",
-        type: "bytes4",
-      },
-    ],
-    stateMutability: "nonpayable",
-  },
-  {
-    type: "function",
-    name: "owner",
-    inputs: [],
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-  },
-  {
-    type: "function",
-    name: "withdrawArtifact",
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "tokenId",
-        type: "uint256",
-      },
-    ],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    type: "event",
-    name: "Demote",
-    inputs: [
-      {
-        name: "demotor",
-        type: "address",
-        indexed: true,
-      },
-      {
-        name: "demoted",
-        type: "address",
-        indexed: true,
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: "event",
-    name: "Deposit",
-    inputs: [
-      {
-        name: "depositor",
-        type: "address",
-        indexed: true,
-      },
-      {
-        name: "tokenId",
-        type: "uint256",
-        indexed: true,
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: "event",
-    name: "Promote",
-    inputs: [
-      {
-        name: "promotor",
-        type: "address",
-        indexed: true,
-      },
-      {
-        name: "promoted",
-        type: "address",
-        indexed: true,
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: "event",
-    name: "Withdrawl",
-    inputs: [
-      {
-        name: "withdrawer",
-        type: "address",
-        indexed: true,
-      },
-      {
-        name: "tokenId",
-        type: "uint256",
-        indexed: true,
-      },
-    ],
-    anonymous: false,
-  },
-];
-const DEPO_ADDRESS = "0xd8c00a439ac617f51e1f8fb58fa7f7334be56f63";
+const isNotShip = (artifact: Artifact) => {
+  return artifact.artifactType < 10;
+};
+const isInWallet = (artifact: Artifact) => {
+  return !artifact.onPlanetId;
+};
 
-const depo: Depo = await df.loadContract(DEPO_ADDRESS, DEPO_ABI);
-export async function getTokenContract() {
-  //@ts-expect-error
-  return df.loadContract(TOKENS_CONTRACT_ADDRESS, TOKENS_APPROVAL_ABI);
+class Plugin {
+  container: HTMLDivElement | undefined;
+
+  PaneController: HTMLDivElement;
+  PaneParent: HTMLDivElement;
+  DepositPane: HTMLDivElement;
+  WithdrawPane: HTMLDivElement;
+  pane: Pane = "loading";
+  constructor() {
+    this.PaneController = document.createElement("div");
+    this.PaneParent = document.createElement("div");
+    this.DepositPane = document.createElement("div");
+    this.WithdrawPane = document.createElement("div");
+    initializeContract();
+  }
+  async initialize() {
+    await initializeContract();
+    this.togglePane("deposit");
+  }
+
+  togglePane(pane: Pane) {
+    this.pane = pane;
+    this.setPaneParentContent();
+  }
+
+  setPaneControllerContent() {
+    this.PaneController!.innerHTML = "";
+    this.PaneController?.append(
+      Button("Deposit", () => {
+        this.togglePane("deposit");
+      })
+    );
+    this.PaneController?.append(
+      Button("Withdraw", () => {
+        this.togglePane("withdraw");
+      })
+    );
+  }
+  setPaneParentContent() {
+    this.PaneParent.childNodes.forEach((n) => this.PaneParent.removeChild(n));
+    if (this.pane == "deposit") {
+      this.PaneParent.append(this.DepositPane);
+    } else {
+      this.PaneParent.append(this.WithdrawPane);
+    }
+  }
+  getArtifactDepositRow(artifact: Artifact): HTMLDivElement {
+    const row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.justifyContent = "space-between";
+
+    row.append(
+      (document.createElement("text").innerHTML = `${artifact.artifactType}`)
+    );
+    row.append(Button("deposit me", () => {}));
+    return row;
+  }
+
+  setDepositPaneContent() {
+    const myArtifacts = df
+      .getMyArtifacts()
+      .filter(isNotShip)
+      .filter(isInWallet);
+    if (myArtifacts.length === 0) {
+      this.DepositPane.innerText = "NO ARTIFACTS TO DEPOSIT";
+    } else {
+      myArtifacts
+        .map(this.getArtifactDepositRow)
+        .forEach((r) => this.DepositPane.append(r));
+    }
+  }
+  setWithdrawPaneContent() {
+    this.WithdrawPane.innerText = "Withdraw";
+  }
+
+  /**
+   * Called when plugin is launched with the "run" button.
+   */
+  async render(container: HTMLDivElement) {
+    this.container = container;
+    container.style.width = "380px";
+    this.setPaneControllerContent();
+    this.setPaneParentContent();
+    this.setDepositPaneContent();
+    this.setWithdrawPaneContent();
+    container.append(this.PaneController!);
+    container.append(this.PaneParent!);
+    // df.getMyArtifacts().filter((a: Artifact) => );
+  }
+
+  /**
+   * Called when plugin modal is closed.
+   */
+  destroy() {}
 }
-const eventHandlers = {
-  ["deposit"]: (addr: string, rawArtifactId: EthersBN) => {
-    console.log("New Artifact is available for withdrawal");
-  },
-  ["withdrawal"]: (addr: string, rawArtifactId: EthersBN) => {
-    console.log(
-      `artifact ${artifactIdFromEthersBN(rawArtifactId)} was withdrawn`
-    );
-  },
-};
-const ethConnection = df.getEthConnection();
-const filters = {
-  address: DEPO_ADDRESS,
-  filters: [
-    depo.filters.Deposit(null, null).topics,
-    depo.filters.Withdrawl(null, null).topics,
-  ],
-};
 
-ethConnection.subscribeToContractEvents(depo, eventHandlers, filters);
-
-const core: DarkForest = df.getContract();
-
-const DepoAPI = {
-  deposit: (artifactId: ArtifactId) => {
-    // Transfers Artifact directly to Depo
-    core["safeTransferFrom(address,address,uint256)"](
-      df.getAccount()!,
-      DEPO_ADDRESS,
-      artifactIdToDecStr(artifactId)
-    );
-  },
-  withdraw: (artifactId: ArtifactId) => {
-    depo.withdrawArtifact(artifactIdToDecStr(artifactId));
-  },
-};
-
-//@ts-ignore
-window.depoContract = depo;
-//@ts-ignore
-window.DEPO = DepoAPI;
+/**
+ * And don't forget to export it!
+ */
+export default Plugin;
